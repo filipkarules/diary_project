a4ea9f03796dbff938fd13038703c9db52167873
mr_akasuna <vadf0957@gmail.com>
2025-05-22 01:24:53 +0300

    PHP recover

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Личный дневник</title>
+    <link rel="stylesheet" href="main7.css">
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <a href="ver1.html">
+                <img src="logo.png" width="100vw" , height="100vh" />
+            </a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href=about.php>Обо мне</a></li>
+                <li><a href=dates.php>Даты</a></li>  
+            </ul>
+        </div>
+        <div class="contacts">
+            <a href="mailto:vadf0957@gmail.com">Написать мне</a>
+        </div>
+    </header>
---
+++ ver8.php
@@ -0,0 +1,36 @@
+    <?php
+    $pageTitle = 'Личный дневник';
+    include '../diary_project/header.php';
+    ?>
+    <main>
+      <div class="content-section">
+        <article class = "custom-text">
+            <h1>Дневник. Версия 8</h1>
+            <p>21.05.2025 23:38</p>
+            <br>Такс, сейчас всю ночь буду делать PHP реализацию кода.
+            <br>Также не стоит забывать про JQuery.
+            <br>Го.
+            <br>Пы сы. Добавил обработчик на кол-во комментариев.
+        </article>
+        <form class="custom-input" id="comment-form">
+            <input class = "name" type="text" placeholder="Имя">
+            <textarea class = "comment-inside" type="text" height = "800px" placeholder="Введите текст..."></textarea>
+            <button type = "submit" class="submit-button">Send</button>
+        </form>
+      </div>
+
+      <div class="comment-section">
+        <h2>Комментарии (<span id = "count-comments"></span>)</h2>
+    </div>
+    <button id="clear-comments">Очистить комментарии</button>
+    </main>
+
+      
+      <?php
+      $pageTitle = 'Личный дневник';
+      include '../diary_project/footer.php';
+      ?>
+      
+      <script src="comments_8.js"></script>
+    </body>
+    </html>
